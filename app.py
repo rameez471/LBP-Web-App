f60ae2335a574ee642adefffbef4f3706f7c710f
rameez471 <yashpathak29@gmail.com>
2020-11-06 20:37:07 +0530

    WTF

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from utils import *
-from imutils.video import VideoStream
+from imutils.video import WebcamVideoStream
 from flask import Response, Flask, render_template, request
 import threading
 import argparse
@@ -8,6 +8,7 @@ import imutils
 import time
 import cv2
 from waitress import serve
+from werkzeug.utils import secure_filename
 
 outputFrame = None
 lock = threading.Lock()
@@ -18,8 +19,8 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 uploads_path = os.path.join(APP_ROOT, 'uploads')
 allowed_set = set(['png', 'jpg', 'jpeg'])
 
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
+# vs = VideoStream(src=0).start()
+# time.sleep(2.0)
 
 # @app.route('/')
 # def index():
@@ -158,7 +159,108 @@ def get_image():
             status='POST http method required.'
         )
 
+@app.route('/predictImage',methods=['GET', 'POST'])
+def predict_image():
 
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return render_template(
+                template_name_or_list='warning.html',
+                status='No file in POST request'
+            )
+
+        file = request.files['file']
+        filename = file.filename
+
+        if filename == '':
+            return render_template(
+                template_name_or_list='warning.html',
+                status='No selected file!'
+            )
+
+        if file and allowed_file(filename=filename, allowed_set=allowed_set):
+            img = cv2.imread(name=file, mode='RGB')
+            faces = faceObj.detect_face(img)
+
+            if faces is not None:
+                (x,y,w,h) = faces[0]
+                face = img[y:y+h, x:x+w]
+                prediction = faceObj.recognize(face)
+                if prediction is not None:
+                    return render_template(
+                        template_name_or_list='predict_result.html',
+                        identity=prediction
+                    )
+                else:
+                    return render_template(
+                        template_name_or_list='predict_result.html',
+                        identity='No identity found!'
+                    )
+            else:
+                return render_template(
+                    template_name_or_list='predict_result.html',
+                    identity='Operation unseccesfull! No human face detected!'
+                )
+
+    else:
+        return render_template(
+            template_name_or_list='warning.html',
+            status='POST HTTP method required.'
+        )
+            
+@app.route('/live',methods=['GET'])
+def face_detect_live():
+    try:
+        cap = WebcamVideoStream(src=0).start()
+        
+        while True:
+            fame_orig = cap.read()
+
+            frame = cv2.resize(src=frame_orig, dsize=(0, 0), fx=0.5, fy=0.5)
+
+            frame = frame[:, :, ::-1]
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+            faces = faceObj.detect_face(frame)
+
+            if faces is not None:
+                for (x,y,w,h) in faces:
+                    cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+
+                    image = gray[y:y+h, x:x+w]
+
+                    prediction = face.recognize(image)
+
+                    if prediction is not None:
+                        cv2.putText(frame,'%s - %.0f' % (prediction),(x-10, y-10), cv2.FONT_HERSHEY_PLAIN,1,(0, 255, 0))
+                    else:
+                        cv2.putText(frame,'not recognized',(x-10, y-10), cv2.FONT_HERSHEY_PLAIN,1,(0, 255, 0))
+
+                cv2.imshow(winname='Video', mat=frame_orig)
+            else:
+                continue
+
+        cap.stop()
+        cv2.destroyAllWindows()
+
+        return render_template(template_name_or_list='index.html')
+
+    except Exception as e:
+        print(e)
+        return render_template(
+            template_name_or_list='warning.html',
+            status = 'No face in database.'
+        )
+
+@app.route('/')
+def index_page():
+    return render_template(template_name_or_list='index.html')
+
+@app.route('/predict')
+def predict_page():
+    return render_template(template_name_or_list='predict.html')
 
 if __name__ == '__main__':
 
